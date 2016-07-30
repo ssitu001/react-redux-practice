23892c4d4fd04a1d40cc08ccc80fe38463b1fb22
Simon Situ <ssitu001@gmail.com>
2016-07-29 18:13:44 -0700

    practice components and state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyC43vMDIVtvF1goOdr22hCjpVcjS4AxRpU';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('BMW');
+
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {     
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      }); //or this.setState({ videos });
+    });  
+  }
+
+  render() {
+    //throttle user search to 300ms
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={ videoSearch } />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo: selectedVideo})}
+          videos={this.state.videos}/>
+      </div>
+    );   
+  }    
+}
+
+
+
+
+
+//Take this component's generated HTML and put it on the page in the DOM
+ReactDOM.render(<App />, document.querySelector(".container"));
